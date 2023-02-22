18f3375147470720709fc56fb107cdf3df82e5fa
HaleyQuirin <haley_quirin1@baylor.edu>
2023-02-22 13:28:21 -0600

    Final edits

--- person.py
+++ person.py
@@ -4,22 +4,32 @@ class Person:
         self.__address = address
         self.__phonenumber = phonenumber
 
+    def get_name(self):
+        return self.__name
+
+    def get_address(self):
+        return self.__address
+
+    def get_phonenumber(self):
+        return self.__phonenumber
+
     def print_person(self):
-        print("Name: ", self.__name)
-        print("Address: ", self.__address)
-        print("Phone Number: ", self.__phonenumber)
+        print("Name:", self.__name)
+        print("Address:", self.__address)
+        print("Phone Number:", self.__phonenumber)
 
 
 class Customer(Person):
-    def __init__(self, name, address, phonenumber, customer_number, boolean_data):
-        self.__name = name
-        self.__address = address
-        self.__phonenumber = phonenumber
+    def __init__(self, name, address, phonenumber, customer_number, on_list):
+        Person.__init__(self, name, address, phonenumber)
         self.__customer_number = customer_number
-        self.__boolean_data_attribute = boolean_data
+        self.__on_list = on_list
 
     def print_person(self):
-        print("Name: ", self.__name)
-        print("Address: ", self.__address)
-        print("Phone Number: ", self.__phonenumber)
-        print("Customer Number: ", self.__customer_number)
+        Person.print_person(self)
+
+        print(f"Customer Number: {self.__customer_number}")
+        if self.__on_list:
+            print("On Mailing List: YES")
+        else:
+            print("On Mailing List: NO")
--- person_demo.py
+++ person_demo.py
@@ -1,1 +1,19 @@
 import person as p
+
+name = "Haley"
+address = "1902 s 12th street"
+phonenumber = "281-917-4734"
+customer_number = 1234
+on_list_flag = True
+
+person1 = p.Person(name, address, phonenumber)
+
+customer1 = p.Customer(name, address, phonenumber, customer_number, on_list_flag)
+
+person1.print_person()
+
+print()
+print()
+print()
+
+customer1.print_person()
